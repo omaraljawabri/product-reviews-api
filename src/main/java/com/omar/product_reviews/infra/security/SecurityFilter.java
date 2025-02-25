37c53d201cc991ee6c41a92e62389f366dd31b31
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-25 16:57:22 -0300

    add Spring Security and JWT Token configuration

---
+++ src/main/java/com/omar/product_reviews/infra/security/SecurityFilter.java
@@ -0,0 +1,46 @@
+package com.omar.product_reviews.infra.security;
+
+import com.omar.product_reviews.entities.User;
+import com.omar.product_reviews.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recoverToken(request);
+        if (token != null){
+            String email = tokenService.validateToken(token);
+            User user = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException(String.format("User with email: %s, not found", email)));
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null){
+            return null;
+        }
+        return authorization.replace("Bearer ", "");
+    }
+}
