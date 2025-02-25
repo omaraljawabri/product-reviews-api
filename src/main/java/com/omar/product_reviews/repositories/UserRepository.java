679eab0b9f1846e1bd6ab8f11ae8530feca91625
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-25 16:57:43 -0300

    create UserRepository

---
+++ src/main/java/com/omar/product_reviews/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.omar.product_reviews.repositories;
+
+import com.omar.product_reviews.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
