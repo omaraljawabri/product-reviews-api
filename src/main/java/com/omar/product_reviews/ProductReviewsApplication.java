1cd067da36d9dff21f90ec52a32456659fa141e0
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-25 18:33:17 -0300

    enable jpa auditing to add created date annotation

--- src/main/java/com/omar/product_reviews/ProductReviewsApplication.java
+++ src/main/java/com/omar/product_reviews/ProductReviewsApplication.java
@@ -2,8 +2,10 @@ package com.omar.product_reviews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ProductReviewsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/omar/product_reviews/entities/User.java
+++ src/main/java/com/omar/product_reviews/entities/User.java
@@ -4,20 +4,24 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.List;
+import java.util.Collections;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "tb_user")
 public class User implements UserDetails {
 
@@ -42,7 +46,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return Collections.emptyList();
     }
 
     @Override
