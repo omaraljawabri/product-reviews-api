a584f562cf47879719babd36f6666caa42e6abc8
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-02-26 14:43:02 -0300

    enable auditing on mongodb to have createdAt and updatedAt informations

--- src/main/java/com/omar/product_reviews/ProductReviewsApplication.java
+++ src/main/java/com/omar/product_reviews/ProductReviewsApplication.java
@@ -3,9 +3,11 @@ package com.omar.product_reviews;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableMongoAuditing
 public class ProductReviewsApplication {
 
 	public static void main(String[] args) {
