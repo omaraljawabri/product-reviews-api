9283a7874ec9e1eefaf74c41b5801885357561cb
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-03-05 17:37:06 -0300

    add some validations for ReviewRequestDTO

--- src/main/java/com/omar/product_reviews/dtos/request/ReviewRequestDTO.java
+++ src/main/java/com/omar/product_reviews/dtos/request/ReviewRequestDTO.java
@@ -1,8 +1,13 @@
 package com.omar.product_reviews.dtos.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record ReviewRequestDTO(
+        @NotNull(message = "product id is required")
         String productId,
+        @NotNull(message = "review rating is required")
         Double rating,
+        @NotNull(message = "review comment is required")
         String comment
 ) {
 }
